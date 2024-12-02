5a3e69e2cf6174332303d51f3a6bbeb222d50c06
Serhii Mordvaniuk <sm636332@gmail.com>
2024-12-02 12:50:23 +0100

    asd

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,17 +1,13 @@
-// src/Product.jsx
+let lalala = 4;
 
-export default function Product() {
-    const price = 12345;
-    const imgUrl = "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
-    return (
-        <>
-            <h2>Tacos</h2>
-            <img src={imgUrl} alt="Tacos With Lime"  width="640"/>
-            <p>Price: { price} credits</p>
-            <label htmlFor="">
-                <input type="text" />
-            </label>
-            
-        </>
-    );
+export default function Product({name, imgUrl = "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640", price}) {
+  return (
+    <div>
+      <h2>{name}</h2>
+	  <img src={imgUrl} alt={name} width="480" />
+      <p>Price: {price} credits</p>
+      <p>{ lalala > 5 ? `yes` : `no`}</p>
+    </div>
+    
+  );
 };
